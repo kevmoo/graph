ee7116943f0e4039f17751b7b82c4dff68891cff
Kevin Moore <kevmoo@google.com>
2018-10-18 10:29:28 -0700

    small tweaks to Edge

--- lib/src/edge.dart
+++ lib/src/edge.dart
@@ -1,13 +1,15 @@
-class Edge<K, D> {
-  final K target;
-  final D data;
+class Edge<Key, Data> {
+  final Key target;
+  final Data data;
 
-  Edge(this.target, {this.data});
+  Edge(this.target, {this.data}) {
+    assert(target != null, 'target cannot be null');
+  }
 
   @override
   bool operator ==(Object other) =>
       other is Edge && other.target == target && other.data == data;
 
   @override
-  int get hashCode => target.hashCode * 31 + data.hashCode;
+  int get hashCode => target.hashCode * 31 ^ data.hashCode;
 }
