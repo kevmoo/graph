4ef176e0a7e23789ba1e28e172212aa2c437fadb
Kevin Moore <kevmoo@google.com>
2018-12-14 15:23:26 -0800

    Add another strongly-connected test

--- test/strongly_connected_test.dart
+++ test/strongly_connected_test.dart
@@ -2,9 +2,11 @@ import 'package:graph/graph.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('strongly connected components', () {
+  test('empty', () {
     expect(DirectedGraph().stronglyConnectedComponents(), isEmpty);
+  });
 
+  test('example 1', () {
     final graph = DirectedGraph.fromMap({
       '1': ['0'],
       '0': ['2', '3'],
@@ -13,10 +15,35 @@ void main() {
       '4': null,
     });
 
-    expect(graph.stronglyConnectedComponents(), [
+    final expected = [
       ['4'],
       ['3'],
-      unorderedEquals(['0', '1', '2'])
-    ]);
+      ['0', '1', '2'],
+    ];
+
+    expect(graph.stronglyConnectedComponents(), expected.map(unorderedEquals));
+  });
+
+  test('example 2', () {
+    // https://en.wikipedia.org/wiki/Strongly_connected_component#/media/File:Scc.png
+    // 2018-12-14
+    final graph = DirectedGraph.fromMap({
+      'a': ['b'],
+      'b': ['e', 'f', 'c'],
+      'c': ['d', 'g'],
+      'd': ['h', 'c'],
+      'e': ['a', 'f'],
+      'f': ['g'],
+      'g': ['f'],
+      'h': ['d'],
+    });
+
+    final expected = [
+      ['f', 'g'],
+      ['h', 'd', 'c'],
+      ['b', 'a', 'e'],
+    ];
+
+    expect(graph.stronglyConnectedComponents(), expected.map(unorderedEquals));
   });
 }
